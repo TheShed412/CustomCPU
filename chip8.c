748c7802d77737e11cb3d348d198dee98c07dfef
Tyler <shed412@gmail.com>
2017-03-03 18:19:56 -0500

    Started header file

---
+++ chip8.c
@@ -0,0 +1,8 @@
+#include "my_cpu.h"
+
+CPU cpu;
+
+int main(int argc, char const *argv[]) {
+    cpu.opcode = 0x00ff;
+    return 0;
+}
---
+++ my_cpu.h
@@ -0,0 +1,16 @@
+#ifndef CHIP8
+#define CHIP8
+
+typedef struct {
+    unsigned short  opcode;             /*the current opcode*/
+    unsigned char   memory[4096];       /*ram 4k ;)*/
+    unsigned char   V[2];              /*registers*/
+    unsigned short  pc;                 /*program counter*/
+    unsigned char   _clock;        /*clock triggered at 0, runs at 60hz*/
+} CPU;
+
+typedef struct {
+    unsigned short instructions[1000];      /*ROM for holding instructions. 16-bit instruction set*/
+} ROM;
+
+#endif
