7ccc61b43b40b1d6bd76187e7433059a42f635bb
Tyler <shed412@gmail.com>
2017-03-09 14:09:52 -0500

    Did the alu

--- my_cpu.c
+++ my_cpu.c
@@ -10,8 +10,51 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-/*takes in tbe two registers and does the operation specified*/
+/*
+takes in the two registers and does the operation specified
+returns the result
+won't worry about carry outs or ins
+I might need to edit since there are only two registers.
+I might have it only take one register that we will store
+the result of doing the operation on the two registers.
+Cause that's how the lab went, but this will make it
+easier to reuse in the future.
 
-unsigned short alu(unsigned short reg_1, unsigned short reg_2, unsigned short opcode){
+I dunno  ¯\_(ツ)_/¯
+*/
 
-}
+byte alu(byte reg_1, byte reg_2, byte opcode){
+
+    switch (opcode) {
+        /*Set operation. reg_2 will be an immediate*/
+        case 0x0:
+            reg_1 = reg_2;
+            break;
+
+        /*Add operation*/
+        case 0x2:
+            reg_1 += reg_2;
+            break;
+
+        /*Subtraction operation*/
+        case 0x3:
+            reg_1 -= reg_2;
+            break;
+
+        /*or operation*/
+        case 0x4:
+            reg_1 = reg_1|reg_2;
+            break;
+
+        /*and operation*/
+        case 0x5:
+            reg_1 = reg_1&reg_2;
+            break;
+
+        /*wtf happened*/
+        default:
+            printf("%s\n", "not supported operation");
+    }/*switch*/
+
+    return reg_1;
+}/*alu*/
