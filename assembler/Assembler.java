8d7bc81546c0268b189238108336f9e2607a17bf
Tyler <shed412@gmail.com>
2017-05-09 14:13:05 -0400

    finished constructor commented instruction format

--- assembler/Assembler.java
+++ assembler/Assembler.java
@@ -1,7 +1,16 @@
 public class Assembler
 {
+	/*
+		I want the instructions to be similar to x86:
+
+		op r1 r2 imm*
+		op r1 imm
+
+		the imm will only be there if r2 is a memory address and imm is the offset.
+
+	*/
 	public static void main(String[] args)
 	{
-			
+
 	}
 }//classs
--- assembler/Instruction.java
+++ assembler/Instruction.java
@@ -2,14 +2,19 @@
 public class Instruction
 {
 	int opcode;
-	int register;
+	int registerResult;
+	int registerFrom;
 	int immediate;
 
 	/*
+		The instructions will be similar to x86
 	*/
 
-	public Instruction(String strOp, String strReg, String strImm)
+	public Instruction (String strOp, String strReg1, String strReg2, String strImm)
 	{
-		opcode = Integer.parseInt(strOp);
-	}
+		opcode 			= Integer.parseInt(strOp);
+		registerResult 	= Integer.parseInt(strReg1);
+		registerFrom	= Integer.parseInt(strReg2);
+		immediate		= Integer.parseInt(strImm);
+	}//constructor
 }//Instruction
