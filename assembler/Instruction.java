3ab5e2357745c0f2181fd2d06b8bedec6f96429a
Tyler <shed412@gmail.com>
2017-05-09 14:05:54 -0400

    Started INstruction constructor

--- assembler/Instruction.java
+++ assembler/Instruction.java
@@ -1,11 +1,15 @@
+
 public class Instruction
 {
 	int opcode;
 	int register;
 	int immediate;
 
-	public Instruction(String assembleyLine)
+	/*
+	*/
+
+	public Instruction(String strOp, String strReg, String strImm)
 	{
-		
+		opcode = Integer.parseInt(strOp);
 	}
 }//Instruction
